575b7660b39183fcce47dbe35be289cc52daa5cf
afrantz <andrew.frantz@stjude.org>
2017-08-08 16:52:29 -0500

    Moved URI parsing out of upload.html and into protocol-handler.js. In package.json moved electron out of general depencencies and into dev dependecies. Windows yelled at me saying electron has to be in devdeps.

--- src/protocol-handler.js
+++ src/protocol-handler.js
@@ -14,12 +14,14 @@ app.setAsDefaultProtocolClient("sjcloud");
 /** Windows protocol handler */
 if (os.platform() == "win32") {
   args = process.argv.slice(1);
-  if (args && args[0].search("sjcloud://") != -1) { // app called by sjcloud:// URI
+  if (args[0] && args[0].search("sjcloud://") != -1) { // app called by sjcloud:// URI
     project_name = args[0].replace("sjcloud://", "");
     for (i = 1; i < args.length; i++) { // Concatenate project names with spaces
       project_name = project_name + " " + args[i];
     }
-    project_name = project_name.substring(0, project_name.length - 1); // remove trailing '/'
+    if (project_name.slice(-1) == "/") {
+      project_name = project_name.substring(0, project_name.length - 1); // remove trailing '/'
+    }
     global.sjcloudURI = project_name;
   }
   else {
@@ -33,7 +35,9 @@ app.on("open-url", function (event, url) {
   event.preventDefault();
   if (url && url.search("sjcloud://") != -1) { // app called by sjcloud:// URI
     project_name = url.replace("sjcloud://", "");
-    project_name = project_name.substring(0, project_name.length - 1); // remove trailing '/'
+    if (project_name.slice(-1) == "/") {
+      project_name = project_name.substring(0, project_name.length - 1); // remove trailing '/'
+    }
     global.sjcloudURI = project_name;
   }
   else {
