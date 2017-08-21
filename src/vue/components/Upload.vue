1a08321026ac79d0f1cb3b76441b483b79732815
Edgar Sioson <esioson@stjude.org>
2017-08-21 11:34:05 -0500

    forgot to rebase upload.vue changes

--- src/vue/components/Upload.vue
+++ src/vue/components/Upload.vue
@@ -3,16 +3,6 @@
 		<div class='col-xs-4 left-panel-container'>
 			<left-panel></left-panel>
 		</div>
-		
-<<<<<<< Updated upstream:src/vue/components/Upload.vue
-		<div class='col-xs-8 right-panel-container'>
-			<nav-bar></nav-bar>
-			<file-status v-show='hasFiles'></file-status>
-			<upload-target v-show='!hasFiles'></upload-target>
-			<div class="button-bar">
-				<!--<button class='btn btn-large btn-stjude' v-on:click=''>Upload</button>-->
-				<button class='btn btn-large btn-stjude-warning' v-on:click='deleteFiles'>Delete</button>
-=======
 		<div class='col-xs-8 rightPanel' style='height:100%;'>
 			<nav-bar style="margin-top:18px;"></nav-bar>
 			
@@ -27,7 +17,6 @@
 			<div style='position:absolute; bottom:10px; right: 10px; text-align:right'>
 				<button class='btn btn-primary' v-on:click='uploadFiles'>Upload</button>
 				<button class='btn btn-danger' v-on:click='deleteFiles'>Delete</button>
->>>>>>> Stashed changes:source/vue/components/Upload.vue
 			</div>
 		</div>
 	</div>
