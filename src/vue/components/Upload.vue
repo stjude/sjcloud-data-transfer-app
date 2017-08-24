0cdfc680f3d9d28befae54e994cc098ed6965ec3
Edgar Sioson <esioson@stjude.org>
2017-08-24 11:34:47 -0500

    Organize fakeTools data for easier test inspection; fill-in the upload complete template section;

--- src/vue/components/Upload.vue
+++ src/vue/components/Upload.vue
@@ -3,25 +3,41 @@
 		<div class='col-xs-4 left-panel-container'>
 			<left-panel></left-panel>
 		</div>
-		<div class='col-xs-8 rightPanel'>
-			<nav-bar></nav-bar>
-			
-			<file-status class="middle" v-show='hasTools && hasFiles'></file-status>
-			<!-- <upload-target class="middle" v-show='hasTools && !hasFiles'></upload-target> -->
-			<dropzone class="middle" v-show='hasTools && !hasFiles'></dropzone>
+		<div class='col-xs-8 right-panel-container'>
+			<div v-show='!transferComplete'>
+				<nav-bar></nav-bar>
+				
+				<file-status class="middle" v-show='hasTools && hasFiles'></file-status>
+				<!-- <upload-target class="middle" v-show='hasTools && !hasFiles'></upload-target> -->
+				<dropzone class="middle" v-show='hasTools && !hasFiles'></dropzone>
 
-			<div class="bottom-bar" v-show="hasTools && hasFiles">
-				<div class="bottom-bar-left"></div>
-				<div class="bottom-bar-right">
-					<button class='btn btn-primary btn-stjude download-btn'
-						    v-bind:disabled='!checkedFiles.length'>
-						    <!-- v-on:click='downloadFiles'> -->
-						Upload
+				<div class="bottom-bar" v-show="hasTools && hasFiles">
+					<div class="bottom-bar-left"></div>
+					<div class="bottom-bar-right">
+						<button class='btn btn-primary btn-stjude download-btn'
+							    v-bind:disabled='!checkedFiles.length'>
+							    <!-- v-on:click='downloadFiles'> -->
+							Upload
+						</button>
+						<button class='btn btn-danger btn-stjude-warning cancel-btn'
+							    v-bind:disabled='!checkedFiles.length'>
+							    <!-- v-on:click='cancelFiles'> -->
+								Cancel
+						</button>
+					</div>
+				</div>
+			</div>
+			
+			<div v-show='transferComplete' style='margin-top:100px; text-align:center'>
+				<step-outcome successMessage='Upload complete!' outcome='done'></step-outcome>
+				<div style="margin-top:30px">
+					<button class='btn btn-primary'>
+						<i class='material-icons' style='vertical-align:bottom'>open_in_browser</i>
+						READY TO RUN
 					</button>
-					<button class='btn btn-danger btn-stjude-warning cancel-btn'
-						    v-bind:disabled='!checkedFiles.length'>
-						    <!-- v-on:click='cancelFiles'> -->
-							Cancel
+					<button class='btn btn-primary' v-on:click='resetCheckedFiles'>
+						<i class='material-icons' style='vertical-align:bottom'>cloud_upload</i>
+						UPLOAD MORE DATA
 					</button>
 				</div>
 			</div>
@@ -35,6 +51,7 @@ import NavBar from './NavBar.vue';
 import FileStatus from './FileStatus.vue';
 import UploadTarget from './UploadTarget.vue';
 import Dropzone from './Dropzone.vue';
+import StepOutcome from './StepOutcome.vue'
 
 export default {
 	components: {
@@ -42,7 +59,8 @@ export default {
 		NavBar,
 		FileStatus,
 		UploadTarget,
-		Dropzone
+		Dropzone,
+		StepOutcome
 	},
 	data() {
 		return {}
@@ -57,6 +75,9 @@ export default {
 		checkedFiles() {
 			return this.$store.getters.checkedFiles
 		},
+		transferComplete() {
+			return this.$store.getters.transferComplete
+		}
 	},
 	methods: {
 		uploadFiles() {
@@ -64,6 +85,10 @@ export default {
 		},
 		deleteFiles() {
 
+		},
+		resetCheckedFiles() {
+			console.log('resetCheckedFiles')
+			//this.$store.commit('resetCheckedFiles')
 		}
 	}
 }
