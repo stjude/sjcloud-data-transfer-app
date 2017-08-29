13d3ac27377430fbe9221e72b5319bf4f5236783
Clay McLeod <clay.mcleod@stjude.org>
2017-08-29 11:53:13 -0500

    Removed duplicate mounted method due to merge.

--- src/vue/App.vue
+++ src/vue/App.vue
@@ -27,9 +27,6 @@ export default {
 		window.addEventListener('keydown', this.toggleToolPath)
 		this.$store.commit('setCurrPath', this.$route.path.slice(1))
 	},
-	mounted() {
-		window.VueApp = this;
-	},
 	updated() {
 		this.$store.commit('setCurrPath', this.$route.path.slice(1))
 	},
