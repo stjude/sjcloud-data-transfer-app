29e491d51343c900c73763d778716c0b11abe223
Clay McLeod <clay.mcleod@stjude.org>
2017-09-25 16:31:52 -0500

    Removed examples.

--- examples/webex-open/main.js
+++
@@ -1,7 +0,0 @@
-function openApp() {
-  let openAppLink = document.createElement("a");
-  openAppLink.href = "sjcloud://" + encodeURIComponent("Rapid RNA-Seq");
-  document.body.append(openAppLink);
-  openAppLink.click();
-  document.body.remove(openAppLink);
-}
