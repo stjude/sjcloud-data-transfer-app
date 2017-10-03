f4c766e1f8bac07106f208833f89e31a230754bc
Andrew Frantz <andrew.frantz@stjude.org>
2017-10-03 13:51:25 -0500

    fixed the path that a "see" comment pointed to.

--- app/bin/backend/utils.js
+++ app/bin/backend/utils.js
@@ -140,7 +140,7 @@ module.exports.runCommandSync = function(cmd) {
     } catch (err) {}
 
     // Warning: the dnanexus-shell.ps1 script used to source environment variables
-    // on Windows sends a DNAnexus banner to STDOUT. Banner will be first 3 lines
+    // on Windows sends a DNAnexus banner to STDOUT. Banner will be first 4 lines
     // of STDOUT.
     cmd = "powershell.exe " + cmd;
     return execSync(cmd, {maxBuffer: 10000000});
