d0b6b11f77955e0aba03bb56e56a3dfa61bbb6d0
Edgar Sioson <esioson@stjude.org>
2017-10-06 11:13:13 -0500

    Show tour hint on user install

--- app/src/frontend/vue/App.vue
+++ app/src/frontend/vue/App.vue
@@ -37,7 +37,9 @@ export default {
 	},
 	updated() {
 		this.$store.commit('setCurrPath', this.$route.path.slice(1));
-		//tour.__promptUser(this.$route.path);
+		if (this.$store.getters.tourHint) {
+			tour.__promptUser(this.$route.path);
+		}
 	},
 	mounted() {
 		window.VueApp = this;
