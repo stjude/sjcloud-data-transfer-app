208e7bd332458d0269eec109f3d8ba6031d4e704
Clay McLeod <clay.mcleod@stjude.org>
2017-10-09 09:48:03 -0500

    Added post-packing functionality for build scripts.

---
+++ build/scripts/finalize.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+if [[ $# -ne 1 ]]; then
+	echo "Usage: `basename $0` <productName>"
+	exit 1
+fi
+
+mkdir -p "dist/FINAL/"
+PRODUCT_NAME=$1; shift;
+export IFS=$'\n'
+
+# (1) Move all assets to a "FINAL" folder
+for file in $(find dist/ -name "$PRODUCT_NAME.app" -o -name "$PRODUCT_NAME.exe"); do
+	mv $file dist/FINAL/
+done
+
+# (2) Delete any folders not called "FINAL"
+for dir in $(find dist/ -type d -not -name "FINAL" -maxdepth 1 -mindepth 1); do
+  rm -r $dir
+done
+
+# (3) Move all artifacts to the root "dist" folder.
+mv dist/FINAL/* dist/
+rm -r dist/FINAL
