f5f62ff817a5831bbc962b511e959bb546bb7f23
Andrew Frantz <andrew.frantz@stjude.org>
2017-10-10 18:19:12 -0500

    Removed comment that was no longer true

--- app/bin/backend/utils.js
+++ app/bin/backend/utils.js
@@ -139,9 +139,6 @@ module.exports.runCommandSync = function(cmd) {
       }
     } catch (err) {}
 
-    // Warning: the dnanexus-shell.ps1 script used to source environment variables
-    // on Windows sends a DNAnexus banner to STDOUT. Banner will be first 4 lines
-    // of STDOUT.
     cmd = "powershell.exe " + cmd;
     return execSync(cmd, {maxBuffer: 10000000});
   }
