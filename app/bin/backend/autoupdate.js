fc392c489d1803cc850441f8fd4f8ae632819d37
Clay McLeod <clay.mcleod@stjude.org>
2017-10-13 19:33:22 -0500

    Updated building to include packing of app and dmg.

--- app/bin/backend/autoupdate.js
+++ app/bin/backend/autoupdate.js
@@ -4,28 +4,30 @@ const server = "https://warm-fortress-45813.herokuapp.com/";
 const feed = `${server}/update/${process.platform}/${electron.app.getVersion()}`;
 
 electron.autoUpdater.setFeedURL(feed);
+electron.autoUpdater.checkForUpdates();
 
 setInterval(() => {
   electron.autoUpdater.checkForUpdates();
-}, 60 * 1000);
+}, 15 * 60 * 1000);
 
 electron.autoUpdater.on("error", (error) => {
   logging.error(error);
 });
 
 electron.autoUpdater.on("checking-for-update", () => {
-  logging.debug("Checking for updates...");
+  logging.info("Checking for updates...");
 });
 
 electron.autoUpdater.on("update-available", () => {
-  logging.debug("Update available!");
+  logging.info("Update available!");
 });
 
 electron.autoUpdater.on("update-not-available", () => {
-  logging.debug("Update not available...");
+  logging.info("Update not available.");
 });
 
 electron.autoUpdater.on("update-downloaded", (event, releaseNotes, releaseName) => {
+  logging.info("Update downloaded.");
   const dialogOpts = {
     type: "info",
     buttons: ["Restart", "Later"],
