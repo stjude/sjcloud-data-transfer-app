3322a30f9721fddec90ab364e6ae91d3cecf1894
Andrew Frantz <andrew.frantz@stjude.org>
2017-11-14 17:07:12 -0600

    Allow customAlert pop-ups to display HTML

--- app/src/frontend/vue/components/CustomAlert.vue
+++ app/src/frontend/vue/components/CustomAlert.vue
@@ -1,19 +1,19 @@
 <template>
 	<div class='alert-main-div' v-show="alertType!=''">
 		<div v-show="alertType=='info'" class='alert alert-info'>
-			<span>{{ alertMessage }}</span>
+			<span v-html='alertMessage'>{{ alertMessage }}</span>
 			<div class='material-icons closer' @click.stop='close'>close</div>
 		</div>
 		<div v-show="alertType=='success'" class='alert alert-success'>
-			<span>{{ alertMessage }}</span>
+			<span v-html='alertMessage'>{{ alertMessage }}</span>
 			<div class='material-icons closer' @click.stop='close'>close</div>
 		</div>
 		<div v-show="alertType=='warning'" class='alert alert-warning'>
-			<span>{{ alertMessage }}</span>
+			<span v-html='alertMessage'>{{ alertMessage }}</span>
 			<div class='material-icons closer' @click.stop='close'>close</div>
 		</div>
 		<div v-show="alertType=='danger'" class='alert alert-danger'>
-			<span>{{ alertMessage }}</span>
+			<span v-html='alertMessage'>{{ alertMessage }}</span>
 			<div class='material-icons closer' @click.stop='close'>close</div>
 		</div>
 	</div>
@@ -21,42 +21,42 @@
 
 <script>
 export default {
-	computed: {
-		alertMessage() {
-			return this.$store.getters.alertMessage;
-		},
-		alertType() {
-			return this.$store.getters.alertType;
-		}
-	},
-	methods: {
-		close() {
-			this.$store.commit('byKey',{alertType: ''});
-		}
-	}
-}
+  computed: {
+    alertMessage() {
+      return this.$store.getters.alertMessage;
+    },
+    alertType() {
+      return this.$store.getters.alertType;
+    }
+  },
+  methods: {
+    close() {
+      this.$store.commit("byKey", { alertType: "" });
+    }
+  }
+};
 </script>
 
 <style scoped>
 .alert-main-div {
-	position: absolute;
-	top: 5px;
-	left: 40%;
-	margin-right: 5px;
-	z-index: 9999;
+  position: absolute;
+  top: 5px;
+  left: 40%;
+  margin-right: 5px;
+  z-index: 9999;
 }
 
 .alert {
-	display: inline-block;
-	padding-right: 25px;
+  display: inline-block;
+  padding-right: 25px;
 }
 
 .closer {
-	position: absolute;
-	top: 5px;
-	right: 5px;
-	font-size: 14px;
-	font-weight: 600;
-	cursor: default;
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: default;
 }
 </style>
