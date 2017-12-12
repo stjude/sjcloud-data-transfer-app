93b697e41de2d4654634371499243c717c8f8389
Clay McLeod <clay.l.mcleod@gmail.com>
2017-12-11 18:15:23 -0600

    Changed "issues" to "help".

--- app/src/frontend/vue/components/UserMenu.vue
+++ app/src/frontend/vue/components/UserMenu.vue
@@ -7,11 +7,11 @@
 	    	<span class='material-icons menu-icon'>settings</span>
 	    	Settings
 	    </div>
-	    <div id='issues-btn'
+	    <div id='help-btn'
 	    	class='menu-item'
 	    	@click.stop='fileAnIssue'>
-	    	<span class='material-icons menu-icon' style='color: #b71c1c;'>error_outline</span>
-	    	Issues
+	    	<span class='material-icons menu-icon' style='color: #b71c1c;'>help_outline</span>
+        Help
 	    </div>
 	    <div id='logout-btn'
 	    	class='menu-item'
@@ -25,64 +25,63 @@
 
 <script>
 export default {
-	computed: {
-		showLogoutBtn() {
-			return this.$route.path=='/download' || this.$route.path=='/upload';
-		},
-		showSettingsBtn() {
-			return this.$route.path=='/download' || this.$route.path=='/upload';
-		},
-		isVisible() {
-			return this.$store.getters.menuIsVisible;
-		}
-	},
-	methods: {
-		logout() {
-	      this.$store.commit('closeMenu');
-	      window.dx.logout((err, result) => {
-	      	this.$store.commit('setLoginState','waiting');
-	        this.$router.replace('/login');
-	    	});
-    	},
-		fileAnIssue() {
-			this.$store.commit('closeMenu');
-			window.utils.openExternal('https://stjude.cloud/contact');
-		},
-		openSettings() {
-			this.$store.commit('closeMenu');
-			this.$store.commit('toggleModal');
-		}
-	}
-}
+  computed: {
+    showLogoutBtn() {
+      return this.$route.path == "/download" || this.$route.path == "/upload";
+    },
+    showSettingsBtn() {
+      return this.$route.path == "/download" || this.$route.path == "/upload";
+    },
+    isVisible() {
+      return this.$store.getters.menuIsVisible;
+    }
+  },
+  methods: {
+    logout() {
+      this.$store.commit("closeMenu");
+      window.dx.logout((err, result) => {
+        this.$store.commit("setLoginState", "waiting");
+        this.$router.replace("/login");
+      });
+    },
+    fileAnIssue() {
+      this.$store.commit("closeMenu");
+      window.utils.openExternal("https://stjude.cloud/contact");
+    },
+    openSettings() {
+      this.$store.commit("closeMenu");
+      this.$store.commit("toggleModal");
+    }
+  }
+};
 </script>
 
 <style scoped>
 #sjcda-top-bar-menu {
-	position: absolute;
-	top: 40px;
-	right: 17px;
-	/*width: 100px;*/
-	padding: 10px;
-	z-index: 2000;
-	background-color: #fff;
-	border: 1px solid #ccc;
+  position: absolute;
+  top: 40px;
+  right: 17px;
+  /*width: 100px;*/
+  padding: 10px;
+  z-index: 2000;
+  background-color: #fff;
+  border: 1px solid #ccc;
 }
 
 .menu-item {
-	width: 120px;
-	/*text-align: center;*/
-	cursor: pointer;
-	padding: 5px 10px;
-	line-height: 16px;
+  width: 120px;
+  /*text-align: center;*/
+  cursor: pointer;
+  padding: 5px 10px;
+  line-height: 16px;
 }
 
 .menu-item:hover {
-	background-color: rgba(145, 202, 251, 0.3);
+  background-color: rgba(145, 202, 251, 0.3);
 }
 
-
 .menu-icon {
-	vertical-align: top;
-	font-size: 16px;
+  vertical-align: top;
+  font-size: 16px;
 }
 </style>
