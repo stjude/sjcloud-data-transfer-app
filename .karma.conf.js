b24432853ad98cb43f60dba8447be62c83b88f39
Clay McLeod <clay.l.mcleod@gmail.com>
2017-12-16 18:53:19 -0600

    Hide configuration files behind dotfiles.

--- .karma.conf.js
+++ .karma.conf.js
@@ -1,37 +1,37 @@
-const webpackConfig = require("./webpack.conf.js");
+const webpackConfig = require('./.webpack.conf.js');
 
-module.exports = function(config) {
+module.exports = function (config) {
   // no need to extract css into a file, so
   // to take out ExtractTextPlugin rule for .less,
   // assumed to be the LAST rule in the rules array
   webpackConfig.module.rules[webpackConfig.module.rules.length - 1] = {
     test: /\.less$/,
-    use: ["css-loader", "less-loader"],
+    use: ['css-loader', 'less-loader'],
   };
   // optional plugins are not needed in testing
   webpackConfig.plugins = [];
 
   config.set({
-    browsers: ["Chrome"],
-    frameworks: ["jasmine"],
+    browsers: ['Chrome'],
+    frameworks: ['jasmine'],
     files: [
-      "./app/bin/frontend/app.bundle.css",
-      "./test/frontend/test.index.js",
+      './app/bin/frontend/app.bundle.css',
+      './test/frontend/test.index.js',
       // serve assets, script, data file used in testing
       {
-        pattern: "./app/testdata/*",
+        pattern: './app/testdata/*',
         served: true,
         included: false,
         watched: false,
       },
       {
-        pattern: "./app/img/*",
+        pattern: './app/img/*',
         served: true,
         included: false,
         watched: false,
       },
       {
-        pattern: "./app/bin/frontend/*",
+        pattern: './app/bin/frontend/*',
         served: true,
         included: false,
         watched: false,
@@ -43,16 +43,16 @@ module.exports = function(config) {
     // and must be mapped to the corresponding
     // karma-served files listed in the "files" properties
     proxies: {
-      "/testdata/": "/base/app/testdata/",
-      "/img/": "/base/app/img/",
-      "/bin/frontend/": "/base/app/bin/frontend/",
+      '/testdata/': '/base/app/testdata/',
+      '/img/': '/base/app/img/',
+      '/bin/frontend/': '/base/app/bin/frontend/',
     },
 
     // we will pass the entry file to webpack for bundling.
     preprocessors: {
-      "./test/frontend/*.js": ["webpack"],
-      "*.js": ["webpack"],
-      "*": ["webpack"],
+      './test/frontend/*.js': ['webpack'],
+      '*.js': ['webpack'],
+      '*': ['webpack'],
     },
 
     // use the webpack config
