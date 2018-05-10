789d972590f5bd4c08ad93cc8e1f54951af079c8
Michael Macias <michael.macias@stjude.org>
2018-05-10 11:18:10 -0500

    Remove install step

--- app/src/frontend/vue/substores/storeStart.js
+++ app/src/frontend/vue/substores/storeStart.js
@@ -5,27 +5,11 @@
 export default function(ref) {
   return {
     state: {
-      /** Install * */
-      pythonOnPath: true,
-      downloadStatus: 'Downloading...',
-      installingDependencies: 'waiting',
-
       /** Login * */
       loginState: 'waiting',
       token: '',
     },
     getters: {
-      /** Install * */
-      pythonOnPath(state) {
-        return state.pythonOnPath;
-      },
-      installingDependencies(state, getters) {
-        return state.installingDependencies;
-      },
-      downloadStatus(state, getters) {
-        return state.downloadStatus;
-      },
-
       /** Login * */
       loginState(state, getters) {
         return state.loginState;
@@ -35,17 +19,6 @@ export default function(ref) {
       },
     },
     mutations: {
-      /** Install * */
-      setPythonOnPath(state, onPath) {
-        state.pythonOnPath = onPath;
-      },
-      setInstallingDependencies(state, installing) {
-        state.installingDependencies = installing;
-      },
-      setDownloadStatus(state, status) {
-        state.downloadStatus = status;
-      },
-
       /** Login * */
       setLoginState(state, status) {
         state.loginState = status;
