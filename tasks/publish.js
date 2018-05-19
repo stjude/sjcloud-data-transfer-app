98461e90597cd8d4be387265816bed5f88ec4fb0
Clay McLeod <clay.l.mcleod@gmail.com>
2018-05-19 15:12:02 -0500

    Experiment with new build process.

--- tasks/publish.js
+++ tasks/publish.js
@@ -13,37 +13,42 @@ const readFile = util.promisify(fs.readFile);
 
 function bumpVersion(level, cb) {
   exec('git rev-parse --abbrev-ref HEAD')
-    .then((result) => {
-      const { stdout } = result;
+    .then(result => {
+      const {stdout} = result;
       const [branch] = stdout.split('\n');
       if (branch !== 'development') {
         throw new Error('Branch must be "development" when bumping version!');
       }
 
-      gulp.src('./package.json')
-        .pipe(bump({ type: level }))
+      gulp
+        .src('./package.json')
+        .pipe(bump({type: level}))
         .pipe(gulp.dest('./'))
         .on('end', () => {
           exec('git add package.json')
             .then(() => readFile('./package.json', 'utf-8'))
-            .then((contents) => {
-              const { version } = JSON.parse(contents);
+            .then(contents => {
+              const {version} = JSON.parse(contents);
               return exec(`git commit -m "Bumped to version ${version}"`);
             })
             .then(() => {
               console.log();
-              console.log('Your version bump has now been successfully committed to git.');
-              console.log('However, you\'ll need to do a "git push" to see the change in Github');
+              console.log(
+                'Your version bump has now been successfully committed to git.'
+              );
+              console.log(
+                'However, you\'ll need to do a "git push" to see the change in Github'
+              );
               console.log();
               cb();
             })
-            .catch((error) => {
+            .catch(error => {
               console.log(error.stack);
               process.exit(1);
             });
         });
     })
-    .catch((error) => {
+    .catch(error => {
       console.log(error.stack);
       process.exit(1);
     });
