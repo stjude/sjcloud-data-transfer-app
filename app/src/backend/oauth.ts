c45f18bd70891c1e17b776aaf727103a9de70551
Michael Macias <michael.macias@stjude.org>
2018-06-06 16:00:48 -0500

    Refactor oauth

--- app/src/backend/oauth.ts
+++ app/src/backend/oauth.ts
@@ -1,35 +1,61 @@
 /**
  * @module oauth
- * @description Contains all OAuth functionality for the application.
- *
+ * @description Handlers to get an access token from DNAnexus.
  */
 
-const ui = require('./ui');
-const pem = require('pem');
-const $ = require('jquery');
-const https = require('https');
-const express = require('express');
-const ipcRenderer = require('electron').ipcRenderer;
+import * as https from 'https';
+
+import * as express from 'express';
+import * as $ from 'jquery';
+
+import { SuccessCallback } from './types';
+import { createOAuthWindow } from './ui';
+import { selfSigned, ICertificate } from './utils';
+
+const PORT = 4433;
+const DX_TOKEN_ENDPOINT = 'https://auth.dnanexus.com/oauth2/token';
+
+interface ITokenResult {
+  access_token: string;
+}
+
+const generateCerts = (): Promise<ICertificate> => {
+  return new Promise((resolve, reject) => {
+    selfSigned((err, certs) => {
+      if (err || !certs) {
+        reject(err);
+      } else {
+        resolve(certs);
+      }
+    });
+  });
+};
 
 /**
- * Opens an Oauth window with the appropriate URL then waits for the user
- * to log in. If the user logs in successfully, the code parameter will be
- * passed to the callback.
+ * Opens a browser window to log in to DNAnexus.
  *
- * @param showInternalURL Show internal (St. Jude) or external URL.
+ * @param showInternalURL Whether to use St. Jude SSO (true) or DNAnexus (false).
  * @param callback
  */
-function waitForCode(
+export const waitForCode = (
   showInternalURL: boolean,
-  callback: any,
-  dryrun: boolean = false,
-) {
-  ui.createOAuthWindow(showInternalURL, (window: any) => {
+  cb: SuccessCallback<string>,
+) => {
+  createOAuthWindow(showInternalURL, async window => {
     if (!window) {
-      return callback(new Error('Could not create window!'), null);
+      cb(new Error('Could not create window!'), null);
+      return;
     }
 
-    const certs: any = ipcRenderer.sendSync('sync/generate-selfsigned');
+    let certs: ICertificate;
+
+    try {
+      certs = await generateCerts();
+    } catch (e) {
+      cb(e, null);
+      return;
+    }
+
     let app = express();
 
     let server = https.createServer(
@@ -45,7 +71,7 @@ function waitForCode(
       window = null;
     });
 
-    app.get('/authcb', function(req: any, res: any) {
+    app.get('/authcb', function(req) {
       // NOTE: Do not set window to null inside the next code block.
       // Here be dragons and mystical code errors that will cause you hours of
       // debugging pain.
@@ -53,35 +79,44 @@ function waitForCode(
         window.close();
       }
 
-      return callback(null, req.query.code);
+      return cb(null, req.query.code);
     });
 
-    server.listen(4433);
-    console.log('Running OAuth listener on port 4433.');
+    server.listen(PORT);
+    console.log(`Running OAuth listener on port ${PORT}.`);
   });
-}
+};
 
 /**
- * Performs the full process of getting a token from DNAnexus using OAuth, both
- * for internal (St. Jude) or external users.
+ * Hooks to a browser window that waits for an access token from DNAnexus.
  *
- * @param showInternalURL Show internal (St. Jude) or external URL.
- * @param callback
+ * @param showInternalURL Whether to use St. Jude SSO (true) or DNAnexus (false).
+ * @param cb
  */
-export function getToken(showInternalURL: boolean, callback: any) {
-  waitForCode(showInternalURL, function(err: any, code: any) {
-    if (err) return callback(err, null);
-    $.post(
-      'https://auth.dnanexus.com/oauth2/token',
-      {
-        grant_type: 'authorization_code',
-        code: code,
-        redirect_uri: 'https://localhost:4433/authcb',
-      },
-      (data: any) => {
-        let authToken = data.access_token;
-        return callback(null, authToken);
-      },
-    );
+export const getToken = (
+  showInternalURL: boolean,
+  cb: SuccessCallback<string>,
+) => {
+  waitForCode(showInternalURL, function(err, code) {
+    if (err || !code) {
+      cb(err, null);
+      return;
+    }
+
+    const payload = {
+      grant_type: 'authorization_code',
+      code: code,
+      redirect_uri: 'https://localhost:4433/authcb',
+    };
+
+    $.post(DX_TOKEN_ENDPOINT, payload)
+      .done((data: ITokenResult) => {
+        const { access_token: accessToken } = data;
+        cb(null, accessToken);
+      })
+      .fail((_jqxhr, textStatus, errorThrown) => {
+        const message = `${textStatus}: ${errorThrown}`;
+        cb(new Error(message), null);
+      });
   });
-}
+};
