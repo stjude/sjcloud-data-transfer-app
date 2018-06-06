a4b4b497c29f7626d9e2cecd9d3e68633bce1f1f
Jobin Sunny <Jobin.Sunny@stjude.org>
2018-06-06 09:54:00 -0500

    Updates to the test stubs

--- src/main/state.spec.ts
+++ src/main/state.spec.ts
@@ -10,15 +10,18 @@ import * as dx from './dx';
 describe('Get state', () => {
   let loggingDebugStub: SinonStub;
   let reportBugStub: SinonStub;
+  let consoleErrorStub: SinonStub;
 
   beforeEach(() => {
     loggingDebugStub = sinon.stub(logging, 'debug');
     reportBugStub = sinon.stub(utils, 'reportBug');
+    consoleErrorStub = sinon.stub(console, 'error');
   });
 
   afterEach(() => {
     loggingDebugStub.restore();
     reportBugStub.restore();
+    consoleErrorStub.restore();
   });
 
   it('should return unknown for invalid platform', () => {
@@ -53,10 +56,14 @@ describe('Get state', () => {
       expect(result).toEqual({ path: 'login' });
     });
 
+    initSJCloudHomeStub.restore();
     loggedInStub.restore();
   });
 
   it('should return upload if logged into DNAnexus', () => {
+    let initSJCloudHomeStub = sinon
+      .stub(utils, 'initSJCloudHome')
+      .returns(null, 'Result');
     let loggedInStubWithArgsStub = sinon.stub(dx, 'loggedIn');
     loggedInStubWithArgsStub.withArgs('testToken').returns(null, 'Result');
 
@@ -64,6 +71,7 @@ describe('Get state', () => {
       expect(result).toEqual({ path: 'upload' });
     });
 
+    initSJCloudHomeStub.restore();
     loggedInStubWithArgsStub.restore();
   });
 });
