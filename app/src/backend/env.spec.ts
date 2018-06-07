ecf4d7010ee26638dca0f720d960962b314ae0fa
Michael Macias <michael.macias@stjude.org>
2018-06-07 13:14:18 -0500

    Add tests for env
    Picked from 1 commit:
    
    commit 2368206fb1691129f273eaecbbf8096f90c036e8
    Author: Jobin Sunny <Jobin.Sunny@stjude.org>
    Date:   Tue Jun 5 15:30:32 2018 -0500
    
        Added test spec for env

---
+++ app/src/backend/env.spec.ts
@@ -0,0 +1,60 @@
+import { getEnv, isDevelopment, isProduction, isTesting } from './env';
+
+let previousNodeEnv: string;
+
+beforeEach(() => {
+  previousNodeEnv = process.env.NODE_ENV;
+});
+
+afterEach(() => {
+  process.env.NODE_ENV = previousNodeEnv;
+});
+
+describe('getEnv', () => {
+  describe('NODE_ENV is set', () => {
+    it('returns the lowercase version of NODE_ENV', () => {
+      process.env.NODE_ENV = 'TESTING';
+      expect(getEnv()).toBe('testing');
+    });
+  });
+
+  describe('NODE_ENV is not set', () => {
+    it('defaults to production', () => {
+      delete process.env.NODE_ENV;
+      expect(getEnv()).toBe('production');
+    });
+  });
+});
+
+describe('isDevelopment', () => {
+  it('returns whether NODE_ENV is set to dev[elopment]', () => {
+    process.env.NODE_ENV = 'development';
+    expect(isDevelopment()).toBe(true);
+    process.env.NODE_ENV = 'dev';
+    expect(isDevelopment()).toBe(true);
+    process.env.NODE_ENV = 'production';
+    expect(isDevelopment()).toBe(false);
+  });
+});
+
+describe('isProduction', () => {
+  it('returns whether NODE_ENV is set to prod[uction]', () => {
+    process.env.NODE_ENV = 'production';
+    expect(isProduction()).toBe(true);
+    process.env.NODE_ENV = 'prod';
+    expect(isProduction()).toBe(true);
+    process.env.NODE_ENV = 'development';
+    expect(isProduction()).toBe(false);
+  });
+});
+
+describe('isTesting', () => {
+  it('returns whether NODE_ENV is set to test[ing]', () => {
+    process.env.NODE_ENV = 'testing';
+    expect(isTesting()).toBe(true);
+    process.env.NODE_ENV = 'test';
+    expect(isTesting()).toBe(true);
+    process.env.NODE_ENV = 'production';
+    expect(isTesting()).toBe(false);
+  });
+});
